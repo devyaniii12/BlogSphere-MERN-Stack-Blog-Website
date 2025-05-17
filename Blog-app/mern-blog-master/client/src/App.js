043722fa5e11a71ab28fcf71ea51ea15b2ffae80
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-05-17 12:14:05 +0530

    Update App.js

--- Blog-app/mern-blog-master/client/src/App.js
+++ Blog-app/mern-blog-master/client/src/App.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage";  
 import RegisterPage from "./pages/RegisterPage";  
 import {UserContextProvider} from "./UserContext";
 import CreatePost from "./pages/CreatePost"; 
