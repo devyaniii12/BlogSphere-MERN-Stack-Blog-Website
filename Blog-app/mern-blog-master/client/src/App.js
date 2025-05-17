13c1288ac7a25c12a30002afa631875cdd8addfb
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-05-17 12:14:46 +0530

    Update App.js

--- Blog-app/mern-blog-master/client/src/App.js
+++ Blog-app/mern-blog-master/client/src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage />} />  
           <Route path="/create" element={<CreatePost />} />  
           <Route path="/post/:id" element={<PostPage />} />
           <Route path="/edit/:id" element={<EditPost />} />
